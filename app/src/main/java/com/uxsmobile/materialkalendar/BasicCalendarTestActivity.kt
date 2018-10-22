160695882ad2b16f75a5e492e2fed8c5869b8202
Daniel Manrique Lucas <dmanluc91@gmail.com>
2018-10-22 16:27:39 +0200

    Add basic calendar test activity

---
+++ app/src/main/java/com/uxsmobile/materialkalendar/BasicCalendarTestActivity.kt
@@ -0,0 +1,33 @@
+package com.uxsmobile.materialkalendar
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.uxsmobile.materialkalendar.data.KalendarDay
+import com.uxsmobile.materialkalendar.presentation.ui.MaterialKalendar
+import kotlinx.android.synthetic.main.activity_basic_calendar.calendarView
+import kotlinx.android.synthetic.main.activity_basic_calendar.textView
+import org.threeten.bp.format.DateTimeFormatter
+import java.util.Locale
+
+/**
+ * @author   Daniel Manrique Lucas <dmanluc91@gmail.com>
+ * @version  1
+ * @since    19/10/2018.
+ *
+ * Copyright © 2018 UXS Mobile. All rights reserved.
+ */
+class BasicCalendarTestActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AndroidThreeTen.init(this)
+        setContentView(R.layout.activity_basic_calendar)
+        calendarView.setOnDateChangedListener(object : MaterialKalendar.OnDateSelectedListener {
+            override fun onDateSelected(widget: MaterialKalendar, day: KalendarDay) {
+                textView.text = day.date.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.getDefault()))
+            }
+        })
+    }
+
+}
